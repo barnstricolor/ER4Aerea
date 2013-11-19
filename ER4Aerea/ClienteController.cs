ccfeb7228842b1274b39b230e4f610f8885d2582
rollan.paiva <rollan_paiva@hotmail.com>
2013-11-19 21:40:50 -0200

    Campos acrescentados

--- ER4Aerea/ClienteController.cs
+++ ER4Aerea/ClienteController.cs
@@ -26,12 +26,30 @@ namespace ER4Aerea
         protected override void salvar_Click(object sender, EventArgs e)
         {
             Cliente cliente = null;
-            if (string.IsNullOrEmpty(tela().txtId.Text))
+            if (string.IsNullOrEmpty(tela().txtID.Text))
                 cliente = new Cliente(tela().txtNome.Text, false);
             else
-                cliente = (Cliente)repositorio().obter(int.Parse(tela().txtId.Text));
+                cliente = (Cliente)repositorio().obter(int.Parse(tela().txtID.Text));
 
             cliente.nome = tela().txtNome.Text;
+            cliente.bairro = tela().txtBairro.Text;
+            cliente.celular = tela().txtCel.Text;
+            cliente.cep = tela().txtCep.Text;
+            //cliente.cidade = tela().txtCidade.Text;
+            cliente.cpf = int.Parse(tela().txtCpf.Text);
+            cliente.email = tela().txtEmail.Text;
+            cliente.endereco = tela().txtEndereco.Text;
+            cliente.id = int.Parse(tela().txtID.Text);
+            cliente.ocupacao = tela().txtOcup.Text;
+            cliente.renda = int.Parse(tela().txtRenda.Text);
+            if (tela().chkMasculino.Checked)
+                cliente.sexo = "M";
+            else
+                cliente.sexo = "F";
+            cliente.promocao = bool.Parse(tela().chkReceb.Text);
+            cliente.numero = int.Parse(tela().txtNum.Text);
+            cliente.telefone = tela().txtTel.Text;
+            cliente.rg = tela().txtRg.Text;         
             repositorio().salvar(cliente);
             tela().Close();
             pesquisaTela.btnPesquisar.PerformClick();
@@ -42,7 +60,7 @@ namespace ER4Aerea
             Cliente cliente = (Cliente)repositorio().obter(id);
 
             criarTela();
-            tela().txtId.Text = cliente.id.ToString();
+            tela().txtID.Text = cliente.id.ToString();
             tela().txtNome.Text = cliente.nome;
             tela().ShowDialog();
         }
