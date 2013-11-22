bfee5530a971af714a6781d4898142be701b0f4c
rollan.paiva <rollan_paiva@hotmail.com>
2013-11-22 16:35:29 -0200

    Max Lenght Alterado

--- ER4Aerea/frmCidade.Designer.cs
+++ ER4Aerea/frmCidade.Designer.cs
@@ -40,18 +40,20 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(12, 105);
+            this.btnSalvar.Location = new System.Drawing.Point(9, 85);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.Size = new System.Drawing.Size(56, 19);
             this.btnSalvar.TabIndex = 2;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             // 
             // btnFechar
             // 
-            this.btnFechar.Location = new System.Drawing.Point(374, 105);
+            this.btnFechar.Location = new System.Drawing.Point(280, 85);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.Size = new System.Drawing.Size(56, 19);
             this.btnFechar.TabIndex = 4;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = true;
@@ -59,9 +61,10 @@
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(93, 105);
+            this.btnLimpar.Location = new System.Drawing.Point(70, 85);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.Size = new System.Drawing.Size(56, 19);
             this.btnLimpar.TabIndex = 3;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -70,49 +73,56 @@
             // lblID
             // 
             this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(12, 4);
+            this.lblID.Location = new System.Drawing.Point(9, 3);
+            this.lblID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(45, 17);
+            this.lblID.Size = new System.Drawing.Size(35, 13);
             this.lblID.TabIndex = 3;
             this.lblID.Text = "Nome";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(15, 24);
+            this.txtNome.Location = new System.Drawing.Point(11, 20);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNome.MaxLength = 50;
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(434, 22);
+            this.txtNome.Size = new System.Drawing.Size(326, 20);
             this.txtNome.TabIndex = 0;
             // 
             // txtCep
             // 
-            this.txtCep.Location = new System.Drawing.Point(15, 69);
+            this.txtCep.Location = new System.Drawing.Point(11, 56);
+            this.txtCep.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCep.MaxLength = 30;
             this.txtCep.Name = "txtCep";
-            this.txtCep.Size = new System.Drawing.Size(72, 22);
+            this.txtCep.Size = new System.Drawing.Size(55, 20);
             this.txtCep.TabIndex = 1;
             this.txtCep.Tag = "N";
             // 
             // lblCep
             // 
             this.lblCep.AutoSize = true;
-            this.lblCep.Location = new System.Drawing.Point(12, 49);
+            this.lblCep.Location = new System.Drawing.Point(9, 40);
+            this.lblCep.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCep.Name = "lblCep";
-            this.lblCep.Size = new System.Drawing.Size(47, 17);
+            this.lblCep.Size = new System.Drawing.Size(37, 13);
             this.lblCep.TabIndex = 5;
             this.lblCep.Text = "C.E.P.";
             // 
             // txtId
             // 
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(377, 69);
+            this.txtId.Location = new System.Drawing.Point(283, 56);
+            this.txtId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(72, 22);
+            this.txtId.Size = new System.Drawing.Size(55, 20);
             this.txtId.TabIndex = 7;
             // 
             // frmCidade
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 137);
+            this.ClientSize = new System.Drawing.Size(346, 111);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.txtCep);
             this.Controls.Add(this.lblCep);
@@ -121,6 +131,7 @@
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.btnSalvar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmCidade";
             this.Text = "frmCidade";
             this.ResumeLayout(false);
