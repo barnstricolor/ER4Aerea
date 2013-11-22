7499ceb4fe3a324aad702a4cd491f141ea8100ff
rollan.paiva <rollan_paiva@hotmail.com>
2013-11-22 16:40:41 -0200

    Max Lenght Alterado

--- ER4Aerea/frmUsuario.Designer.cs
+++ ER4Aerea/frmUsuario.Designer.cs
@@ -61,10 +61,9 @@
             // nOM_ENDERECOLabel
             // 
             nOM_ENDERECOLabel.AutoSize = true;
-            nOM_ENDERECOLabel.Location = new System.Drawing.Point(199, 43);
-            nOM_ENDERECOLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            nOM_ENDERECOLabel.Location = new System.Drawing.Point(149, 35);
             nOM_ENDERECOLabel.Name = "nOM_ENDERECOLabel";
-            nOM_ENDERECOLabel.Size = new System.Drawing.Size(73, 17);
+            nOM_ENDERECOLabel.Size = new System.Drawing.Size(56, 13);
             nOM_ENDERECOLabel.TabIndex = 3;
             nOM_ENDERECOLabel.Tag = "";
             nOM_ENDERECOLabel.Text = "Endereço:";
@@ -72,10 +71,9 @@
             // nOM_USUARIOLabel
             // 
             nOM_USUARIOLabel.AutoSize = true;
-            nOM_USUARIOLabel.Location = new System.Drawing.Point(16, 10);
-            nOM_USUARIOLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            nOM_USUARIOLabel.Location = new System.Drawing.Point(12, 8);
             nOM_USUARIOLabel.Name = "nOM_USUARIOLabel";
-            nOM_USUARIOLabel.Size = new System.Drawing.Size(49, 17);
+            nOM_USUARIOLabel.Size = new System.Drawing.Size(38, 13);
             nOM_USUARIOLabel.TabIndex = 5;
             nOM_USUARIOLabel.Tag = "";
             nOM_USUARIOLabel.Text = "Nome:";
@@ -83,10 +81,9 @@
             // nOM_SENHALabel
             // 
             nOM_SENHALabel.AutoSize = true;
-            nOM_SENHALabel.Location = new System.Drawing.Point(236, 107);
-            nOM_SENHALabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            nOM_SENHALabel.Location = new System.Drawing.Point(177, 87);
             nOM_SENHALabel.Name = "nOM_SENHALabel";
-            nOM_SENHALabel.Size = new System.Drawing.Size(53, 17);
+            nOM_SENHALabel.Size = new System.Drawing.Size(41, 13);
             nOM_SENHALabel.TabIndex = 7;
             nOM_SENHALabel.Tag = "";
             nOM_SENHALabel.Text = "Senha:";
@@ -94,10 +91,9 @@
             // nUM_CPFLabel
             // 
             nUM_CPFLabel.AutoSize = true;
-            nUM_CPFLabel.Location = new System.Drawing.Point(16, 43);
-            nUM_CPFLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            nUM_CPFLabel.Location = new System.Drawing.Point(12, 35);
             nUM_CPFLabel.Name = "nUM_CPFLabel";
-            nUM_CPFLabel.Size = new System.Drawing.Size(38, 17);
+            nUM_CPFLabel.Size = new System.Drawing.Size(30, 13);
             nUM_CPFLabel.TabIndex = 9;
             nUM_CPFLabel.Tag = "";
             nUM_CPFLabel.Text = "CPF:";
@@ -105,10 +101,9 @@
             // nOM_LOGINLabel
             // 
             nOM_LOGINLabel.AutoSize = true;
-            nOM_LOGINLabel.Location = new System.Drawing.Point(16, 107);
-            nOM_LOGINLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            nOM_LOGINLabel.Location = new System.Drawing.Point(12, 87);
             nOM_LOGINLabel.Name = "nOM_LOGINLabel";
-            nOM_LOGINLabel.Size = new System.Drawing.Size(47, 17);
+            nOM_LOGINLabel.Size = new System.Drawing.Size(36, 13);
             nOM_LOGINLabel.TabIndex = 11;
             nOM_LOGINLabel.Tag = "";
             nOM_LOGINLabel.Text = "Login:";
@@ -116,10 +111,9 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(139, 74);
-            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label1.Location = new System.Drawing.Point(104, 60);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(56, 17);
+            label1.Size = new System.Drawing.Size(43, 13);
             label1.TabIndex = 15;
             label1.Tag = "";
             label1.Text = "Cidade:";
@@ -127,10 +121,9 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(27, 72);
-            label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label3.Location = new System.Drawing.Point(20, 58);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(27, 17);
+            label3.Size = new System.Drawing.Size(22, 13);
             label3.TabIndex = 18;
             label3.Tag = "";
             label3.Text = "Nº:";
@@ -138,67 +131,64 @@
             // txtId
             // 
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(503, 207);
-            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Location = new System.Drawing.Point(377, 168);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(68, 22);
+            this.txtId.Size = new System.Drawing.Size(52, 20);
             this.txtId.TabIndex = 1;
             this.txtId.Tag = "";
             // 
             // txtEndereco
             // 
-            this.txtEndereco.Location = new System.Drawing.Point(281, 39);
-            this.txtEndereco.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEndereco.Location = new System.Drawing.Point(211, 32);
+            this.txtEndereco.MaxLength = 50;
             this.txtEndereco.Name = "txtEndereco";
-            this.txtEndereco.Size = new System.Drawing.Size(167, 22);
+            this.txtEndereco.Size = new System.Drawing.Size(126, 20);
             this.txtEndereco.TabIndex = 2;
             this.txtEndereco.Tag = "";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(76, 7);
-            this.txtNome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNome.Location = new System.Drawing.Point(57, 6);
+            this.txtNome.MaxLength = 50;
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(372, 22);
+            this.txtNome.Size = new System.Drawing.Size(280, 20);
             this.txtNome.TabIndex = 0;
             this.txtNome.Tag = "";
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(299, 103);
-            this.txtSenha.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSenha.Location = new System.Drawing.Point(224, 84);
+            this.txtSenha.MaxLength = 20;
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.PasswordChar = '*';
-            this.txtSenha.Size = new System.Drawing.Size(149, 22);
+            this.txtSenha.Size = new System.Drawing.Size(113, 20);
             this.txtSenha.TabIndex = 6;
             this.txtSenha.Tag = "";
             // 
             // txtCpf
             // 
-            this.txtCpf.Location = new System.Drawing.Point(76, 39);
-            this.txtCpf.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCpf.Location = new System.Drawing.Point(57, 32);
             this.txtCpf.MaxLength = 11;
             this.txtCpf.Name = "txtCpf";
-            this.txtCpf.Size = new System.Drawing.Size(113, 22);
+            this.txtCpf.Size = new System.Drawing.Size(86, 20);
             this.txtCpf.TabIndex = 1;
             this.txtCpf.Tag = "N";
             // 
             // txtLogin
             // 
-            this.txtLogin.Location = new System.Drawing.Point(76, 103);
-            this.txtLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLogin.Location = new System.Drawing.Point(57, 84);
+            this.txtLogin.MaxLength = 20;
             this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(151, 22);
+            this.txtLogin.Size = new System.Drawing.Size(114, 20);
             this.txtLogin.TabIndex = 5;
             this.txtLogin.Tag = "";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(457, 7);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(343, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(196, 188);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 153);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 13;
             this.pictureBox1.TabStop = false;
@@ -206,18 +196,19 @@
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(76, 69);
-            this.txtNumero.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNumero.Location = new System.Drawing.Point(57, 56);
+            this.txtNumero.MaxLength = 5;
             this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(37, 22);
+            this.txtNumero.Size = new System.Drawing.Size(29, 20);
             this.txtNumero.TabIndex = 3;
             this.txtNumero.Tag = "N";
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(93, 206);
+            this.btnLimpar.Location = new System.Drawing.Point(70, 167);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.Size = new System.Drawing.Size(56, 19);
             this.btnLimpar.TabIndex = 8;
             this.btnLimpar.Tag = "";
             this.btnLimpar.Text = "Limpar";
@@ -226,9 +217,10 @@
             // 
             // btnFechar
             // 
-            this.btnFechar.Location = new System.Drawing.Point(578, 206);
+            this.btnFechar.Location = new System.Drawing.Point(434, 167);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.Size = new System.Drawing.Size(56, 19);
             this.btnFechar.TabIndex = 9;
             this.btnFechar.Tag = "";
             this.btnFechar.Text = "Fechar";
@@ -237,9 +229,10 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(12, 206);
+            this.btnSalvar.Location = new System.Drawing.Point(9, 167);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.Size = new System.Drawing.Size(56, 19);
             this.btnSalvar.TabIndex = 7;
             this.btnSalvar.Tag = "";
             this.btnSalvar.Text = "Salvar";
@@ -249,17 +242,18 @@
             // 
             this.dcbCidade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.dcbCidade.FormattingEnabled = true;
-            this.dcbCidade.Location = new System.Drawing.Point(202, 68);
+            this.dcbCidade.Location = new System.Drawing.Point(152, 55);
+            this.dcbCidade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dcbCidade.Name = "dcbCidade";
-            this.dcbCidade.Size = new System.Drawing.Size(248, 24);
+            this.dcbCidade.Size = new System.Drawing.Size(187, 21);
             this.dcbCidade.TabIndex = 4;
             this.dcbCidade.Tag = "";
             // 
             // frmUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 241);
+            this.ClientSize = new System.Drawing.Size(502, 196);
             this.Controls.Add(this.dcbCidade);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnFechar);
@@ -279,7 +273,6 @@
             this.Controls.Add(this.txtCpf);
             this.Controls.Add(nOM_LOGINLabel);
             this.Controls.Add(this.txtLogin);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmUsuario";
             this.Tag = "CAD_USUARIO";
             this.Text = "frmUsuario";
