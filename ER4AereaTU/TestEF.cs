81febcac5ad34a62216ab527eaa7c157d7e553cd
barnstricolor <barnstricolor@gmail.com>
2013-11-26 16:30:54 -0200

    EF Funcionando

---
+++ ER4AereaTU/TestEF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ER4Aerea;
+using ER4Aerea.Model;
+
+namespace ER4AereaTU
+{
+    [TestClass]
+    public class TestEF
+    {
+        
+        [TestMethod]
+        public void ef()
+        {
+            Entities2 e = new Entities2();
+            List<CIDADE> lista = new List<CIDADE>();
+            IQueryable<CIDADE> cidadeQuery = from cidade in e.CIDADE
+                                                select cidade;
+            Console.WriteLine("Cidades:");
+            foreach (var c in cidadeQuery)
+            {
+                Console.WriteLine(c.NOM_CIDADE);
+            }
+        }
+
+
+    }
+}
